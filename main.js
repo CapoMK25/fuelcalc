8946298e7261f867239c8994221817bb3a4b1164
unknown <parisburner6969@gmail.com>
2023-11-22 09:06:00 +0200

    Added javascript source code for calculator

---
+++ main.js
@@ -0,0 +1,7 @@
+// the function here?
+function calc() {
+
+const welcome = "This is my personal carbon footprint calculator written with vanilla JavaScript for now. The calculator will ask you a bunch of questions regarding your living habits."
+let userinput = prompt ("Do you own a car?");
+};
+console.log(calc);
