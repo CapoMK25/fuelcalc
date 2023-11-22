044020c6496079e773c63fdd883fe1cced1c79cc
unknown <parisburner6969@gmail.com>
2023-11-22 10:51:05 +0200

    Changed source code to project

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-// the function here?
-function calc() {
+let res = document.getElementById('result');
+            let res2 = document.getElementById("result2");
+            let res3 = document.getElementById("result3");
+            let res4 = document.getElementById("result4");
+            let mpg = document.getElementById('mpg');
+            let dist = document.getElementById('dist');
+            let trip = document.getElementById('trips');
 
-const welcome = "This is my personal carbon footprint calculator written with vanilla JavaScript for now. The calculator will ask you a bunch of questions regarding your living habits."
-let userinput = prompt ("Do you own a car?");
-};
-console.log(calc);
+
+function calcCO2() {
+    let x = mpg.value;
+    let y = dist.value;
+    let z = trips.value;
+    let calc4 = (8.8/x)*1000;
+    let calc = (calc4 * y)/1000;
+    let calc2 = (calc * z);
+    let calc3 = (calc2*52);
+    calc = calc.toFixed(2);
+    calc2 = calc2.toFixed(2);
+    calc3 = calc3.toFixed(2);
+    calc4 = calc4.toFixed(1);
+    console.log('calc is: ' + calc);
+    res.innerHTML = calc;
+    res2.innerHTML = calc2;
+    res3.innerHTML = calc3;
+    res4.innerHTML = calc4;
+    }
